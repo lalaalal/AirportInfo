d9fab4f88e9a7363e8f193f33421efbbb1edc076
goonhee lim <goon7484@gmail.com>
2022-12-08 21:32:26 +0900

    차트뷰 1단계

--- src/test/java/com/airportinfo/view/AirportChartViewTest.java
+++ src/test/java/com/airportinfo/view/AirportChartViewTest.java
@@ -5,6 +5,8 @@ import com.airportinfo.model.Airport;
 import com.airportinfo.view.airport.AirportChartView;
 import com.airportinfo.view.airport.AttributeStatisticCreator;
 import com.airportinfo.view.chart.HistogramView;
+import com.airportinfo.view.chart.PieChartView;
+import com.airportinfo.view.chart.chartGui.AirportChartContentView;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -16,9 +18,14 @@ public class AirportChartViewTest {
         AirportController airportController = new AirportController();
         airportController.loadFromDB();
 
-        MainFrame mainFrame = new AirportFrame();
-        TestContentView contentView = new TestContentView(mainFrame);
+        //MainFrame mainFrame = new AirportFrame();
+       // TestContentView contentView = new TestContentView(mainFrame);
+
+        AirportFrame mainFrame = new AirportFrame();
+        AirportChartContentView contentView = new AirportChartContentView(mainFrame);
+
         HistogramView histogramView = new HistogramView();
+        PieChartView pieChartView = new PieChartView();
         AirportChartView airportChartView = new AirportChartView(histogramView, new AttributeStatisticCreator(Airport::getRegion));
         contentView.setComponent(airportChartView);
         airportChartView.setAirports(List.of(airportController.getAirports()));
